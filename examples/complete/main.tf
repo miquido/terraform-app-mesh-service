bac9691b88489c1e6f687757a9b772f85033006e
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-05-31 09:56:32 +0000

    feat(ci): Dependabot

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,5 +3,16 @@ provider "aws" {
 }
 
 module "test" {
-  source = "../../"
+  source                   = "../../"
+  app_health_check_path    = "test/api/health"
+  app_port                 = "8080"
+  appmesh_domain           = "mesh.example.com"
+  appmesh_name             = "test"
+  appmesh_service_name     = "test"
+  cloud_map_dns            = "test"
+  cloud_map_hosted_zone_id = "test"
+  cloud_map_namespace_name = "test"
+  map_id                   = "test"
+  task_role_name           = "example_task"
+  zone_id                  = "Z3XAMPL3"
 }
--- variables.tf
+++ variables.tf
@@ -15,17 +15,17 @@ variable "appmesh_service_name" {
 }
 
 variable "cloud_map_dns" {
-  type = string
+  type        = string
   description = "Cloud map dns"
 }
 
 variable "cloud_map_hosted_zone_id" {
-  type = string
+  type        = string
   description = "Cloud map hosted zone id"
 }
 
 variable "zone_id" {
-  type = string
+  type        = string
   description = "Route 53 Cloud map zone id"
 }
 
@@ -36,31 +36,31 @@ variable "tags" {
 }
 
 variable "appmesh_domain" {
-  type = string
+  type        = string
   description = "Appmesh domain"
 }
 
 variable "appmesh_name" {
-  type = string
+  type        = string
   description = "Appmesh name"
 }
 
 variable "app_port" {
-  type = string
+  type        = string
   description = "Appmesh port"
 }
 
 variable "app_health_check_path" {
-  type = string
+  type        = string
   description = "App health check port"
 }
 
 variable "cloud_map_namespace_name" {
-  type = string
+  type        = string
   description = "Cloud map namespace name"
 }
 
 variable "task_role_name" {
-  type = string
+  type        = string
   description = "ECS task role name"
 }
